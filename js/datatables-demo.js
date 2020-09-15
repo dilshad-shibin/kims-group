dfac7af41396b64ccb57ad668c6c06b5c0dc8a2e
dilshad shibin <dilshadkp05@gmail.com>
2020-09-14 17:41:26 -0700

    kims all

---
+++ js/datatables-demo.js
@@ -0,0 +1,10 @@
+// Call the dataTables jQuery plugin
+$(document).ready(function() {
+    $('#dataTable').DataTable();
+});
+
+
+// generic medicines
+$(document).ready(function() {
+    $('#dataTable2').DataTable();
+});
